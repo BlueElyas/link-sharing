4e2e294a4cb49223799a9b82afb3f274ac2f6f7f
Elyas <143660964+BlueElyas@users.noreply.github.com>
2024-01-16 19:33:17 +0000

    Add routing to the app component with appshell and login

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AppShell from "./components/nav/AppShell";
+import Login from "./pages/Login";
+
 function App() {
   return (
-    <div className="App">
-      <h1 >Hello</h1>
-      <p>Hello</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AppShell/>}>
+          <Route index element={<Login/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
