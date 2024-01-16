942b251579c8f23c5ec5dd1799a7e8e88ccfbcfd
Elyas <143660964+BlueElyas@users.noreply.github.com>
2024-01-16 19:32:04 +0000

    Update theme.ts file for mui compononets

--- src/theme.ts
+++ src/theme.ts
@@ -27,6 +27,67 @@ import { createTheme } from "@mui/material";
         ].join(','),
         allVariants: {
             userSelect: 'none'
+        },
+        subtitle2: {
+            fontWeight: 500,
+        }
+    },
+    components: {
+        MuiInputLabel: {
+            styleOverrides: {
+                root: {
+                    backgroundColor: "#FAFAFA",
+                    color: '#333333',
+                    fontSize: '14px',
+                }
+            }
+        },
+        MuiPaper: {
+            styleOverrides: {
+                root: {
+                    boxShadow: 'none',
+                }
+            }
+        },
+        MuiTextField: {
+            styleOverrides: {
+                root: {
+                    borderRadius: '12px',
+                    border: '#D9D9D9',
+                    boxShadow: 'none',
+                }
+            }
+        },
+        MuiInputBase: {
+            styleOverrides: {
+                input: {
+                        padding: '0px'
+                    }     
+            }
+        },
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    fontWeight: 500,
+                    color: '#FFFFFF',
+                    backgroundColor: '#633CFF',
+                    textTransform: 'none',
+                    borderRadius: '8px',
+                    ':hover' : {
+                        boxShadow: '0px 0px 32px 0px rgba(99, 60, 255, 0.25)',
+                        backgroundColor: '#BEADFF'
+                    }
+                }
+            }
+        },
+        MuiLink: {
+            styleOverrides: {
+                root: {
+                    textDecoration: 'none',
+                    color: '#633CFF',
+                    fontSize: '12px'
+                }
+            }
         }
     }
 })
