9bada4cf1e45bdf85153646feb42d5cc922bbbdf
Elyas <143660964+BlueElyas@users.noreply.github.com>
2024-01-17 17:57:00 +0000

    Update routing to incldue createaccount page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppShell from "./components/nav/AppShell";
 import Login from "./pages/Login";
+import CreateActtount from "./pages/CreateAccount";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path='/' element={<AppShell/>}>
           <Route index element={<Login/>} />
+          <Route path="/create-account" element={<CreateActtount/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
