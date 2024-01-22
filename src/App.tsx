de5cd5902742fa5f37f3ac7972d1b77245db36e9
Elyas <143660964+BlueElyas@users.noreply.github.com>
2024-01-22 19:32:09 +0000

    Update routing using LogginShell component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AppShell from "./components/nav/AppShell";
 import Login from "./pages/Login";
 import CreateActtount from "./pages/CreateAccount";
+import CreateLinks from "./pages/CreateLinks";
+import LoggedInShell from "./components/nav/LoggedInShell";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<AppShell/>}>
-          <Route index element={<Login/>} />
-          <Route path="/create-account" element={<CreateActtount/>}/>
+        <Route path='/' element={<LoggedInShell/>}>
+            <Route index path="create-links" element={<CreateLinks/>}/>
+            <Route path="profile" element={<Profile/>}/>
         </Route>
+          <Route path="login" element={<Login/>} />
+          <Route path="create-account" element={<CreateActtount/>}/>
       </Routes>
     </BrowserRouter>
   );
