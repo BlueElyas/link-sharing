a1cb43ae03af5d9a8a5a3a8fce5ce80ba3946cda
Elyas <143660964+BlueElyas@users.noreply.github.com>
2024-02-01 14:40:23 +0000

    Add data for the URLs

---
+++ src/data.tsx
@@ -0,0 +1,86 @@
+ import { ReactComponent as Github } from './assets/images/icon-github.svg'
+ import { ReactComponent as DevTo } from './assets/images/icon-devto.svg'
+ import { ReactComponent as FrontendMentor } from './assets/images/icon-frontend-mentor.svg'
+ import { ReactComponent as Codewars } from './assets/images/icon-codewars.svg'
+ import { ReactComponent as Twitter } from './assets/images/icon-twitter.svg'
+ import { ReactComponent as FreeCodeCamp } from './assets/images/icon-freecodecamp.svg'
+ import { ReactComponent as Linkedin } from './assets/images/icon-linkedin.svg'
+ import { ReactComponent as GitLab } from './assets/images/icon-gitlab.svg'
+ import { ReactComponent as Youtube } from './assets/images/icon-youtube.svg'
+ import { ReactComponent as Hashnode } from './assets/images/icon-hashnode.svg'
+ import { ReactComponent as Facebook } from './assets/images/icon-facebook.svg'
+ import { ReactComponent as StackOverflow } from './assets/images/icon-stack-overflow.svg'
+ import { ReactComponent as Twitch } from './assets/images/icon-twitch.svg'
+
+ 
+ const data = [
+    {
+        name: 'GitHub',
+        image: Github,
+        backgroundColor: '#1A1A1A',
+    },
+    {
+        name: 'Dev.to',
+        image: DevTo,
+        backgroundColor: '#333333',
+    },
+    {
+        name: 'Frontend Mentor',
+        image: FrontendMentor,
+        backgroundColor: '#D9D9D9',
+    },  
+    {
+        name: 'Codewars',
+        image: Codewars,
+        backgroundColor: '#8A1A50',
+    },
+    {
+        name: 'Twitter',
+        image: Twitter,
+        backgroundColor: '#43B7E9',
+    },
+    {
+        name: 'freeCodeCamp',
+        image: FreeCodeCamp,
+        backgroundColor: '#302267',
+    },
+    {
+        name: 'LinkedIn',
+        image: Linkedin,
+        backgroundColor: '#2D68FF',
+    },
+    {
+        name: 'GitLab',
+        image: GitLab,
+        backgroundColor: '#EB4925',
+    },
+    {
+        name: 'Youtube',
+        image: Youtube,
+        backgroundColor: '#EE3939',
+    },
+    {
+        name: 'Hashnode',
+        image: Hashnode,
+        backgroundColor: '#0330D1',
+    },
+    {
+        name: 'Facebook',
+        image: Facebook,
+        backgroundColor: '#2442AC',
+    },
+    {
+        name: 'Stack Overflow',
+        image: StackOverflow,
+        backgroundColor: '#EC7100',
+    },
+    {
+        name: 'Twitch',
+        image: Twitch,
+        backgroundColor: '#EE3FC8'
+    },
+]
+
+ export default data
+
+ 
