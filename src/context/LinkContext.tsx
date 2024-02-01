3e2259110591f3b7f11f5bb3628fe174163d6313
Elyas <143660964+BlueElyas@users.noreply.github.com>
2024-02-01 18:23:33 +0000

    Create link context for adding and removing links

---
+++ src/context/LinkContext.tsx
@@ -0,0 +1,63 @@
+import { useContext, createContext, ReactNode, useState, SetStateAction, ProviderProps } from "react"
+
+type LinkProviderProps = {
+    children: ReactNode
+}
+
+type linkProperties = {
+    platform: string
+    link: string
+    id: number
+}
+ 
+type LinkContextValues = {
+    saveLink: () => void
+    setLinkItems: React.Dispatch<SetStateAction<linkProperties>>
+    addNewLink: () => void
+    newLink: boolean
+    linkList:  linkProperties[]
+}
+
+const LinkContext = createContext({} as LinkContextValues)
+
+
+
+export default function LinkProvider ( { children } : LinkProviderProps ) {
+    const [linkItems, setLinkItems] = useState<linkProperties>( {
+        platform: '',
+        link: '',
+        id: 0
+    } )
+    const [linkList, setLinkList] = useState<linkProperties[]> ( [ ] ) 
+    const [newLink, setNewLink] = useState(false)
+
+    const saveLink = () => {
+        if (linkItems.platform.trim() && linkItems.link.trim()) {
+            setLinkList((prevLinkList) => [...prevLinkList, linkItems])
+
+            setLinkItems( {
+                platform: '',
+                link: '',
+                id: 0
+            } )
+            
+            setNewLink(false)
+        }
+    }
+        
+    const addNewLink = () => {
+        setNewLink(p => !p)
+    }
+
+    console.log(linkList)
+
+    return(
+        <LinkContext.Provider value={ { saveLink, setLinkItems, addNewLink, newLink, linkList } }>
+        {children}
+        </LinkContext.Provider>
+    )
+}
+
+export function useLinkContext() {
+    return useContext(LinkContext)
+} 
