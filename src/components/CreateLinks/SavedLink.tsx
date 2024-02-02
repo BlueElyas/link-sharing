5fd7ca8d1cb69799547bb90adf743bfbd8ac1588
Elyas <143660964+BlueElyas@users.noreply.github.com>
2024-02-02 00:03:19 +0000

    Add removelink function to button

--- src/components/CreateLinks/SavedLink.tsx
+++ src/components/CreateLinks/SavedLink.tsx
@@ -12,17 +12,21 @@ import {  ReactComponent as HamburderIcon } from '../../assets/images/icon-drag-
 import { ReactComponent as LinkIcon } from '../../assets/images/icon-link.svg'
 import theme from "../../theme";
 import  data  from '../../data'
+import { useLinkContext } from "../../context/LinkContext";
 
  
 export default function SavedLink ( { linkValues } : any ) {
     const { platform, id, link } = linkValues
+    const { removeLink } = useLinkContext()
 
     return(
         <Container sx={{backgroundColor: '#FAFAFA', borderRadius: '8px', padding: 2, width: '100%'}}>
             <Stack gap={3} >
                 <Stack direction='row' alignItems='baseline' justifyContent='space-between'>
                     <Typography color={theme.palette.text.secondary} variant="subtitle2"><HamburderIcon/> Link #{id}</Typography>
-                    <Typography color={theme.palette.text.secondary} sx={{opacity: 0.8, }} variant="body2"> Remove</Typography>
+                    <Typography color={theme.palette.text.secondary} sx={{opacity: 0.8, }} variant="body2">
+                        <button  onClick={() => removeLink(id)}>Remove</button>
+                    </Typography>
                 </Stack>
                 <Stack gap={3} draggable>
                             <Stack>
@@ -33,6 +37,7 @@ export default function SavedLink ( { linkValues } : any ) {
                                         value={platform}
                                         onChange={() => {}}  
                                         sx={{backgroundColor: '#fff', display: 'flex', alignItems:'center', gap:3 }}    
+                                        disabled={true}
                                     >
                                         {data.map(links => {
                                             return <MenuItem 
@@ -58,8 +63,7 @@ export default function SavedLink ( { linkValues } : any ) {
                                 InputProps={{
                                     startAdornment: <InputAdornment position="start"><LinkIcon/></InputAdornment>
                                 }}
-                                // onBlur={false}
-                                />
+                            />
                         </Stack>
                     </Stack>
             </Stack>
