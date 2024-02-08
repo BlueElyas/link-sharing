dfe4bb7dd6def9eac517d5a88ff4b5bc4ea24cb1
Elyas <143660964+BlueElyas@users.noreply.github.com>
2024-02-08 00:16:01 +0000

    Update routing with new context and protected route

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,27 @@ import CreateActtount from "./pages/CreateAccount";
 import CreateLinks from "./pages/CreateLinks";
 import LoggedInShell from "./components/nav/LoggedInShell";
 import Profile from "./pages/Profile";
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./components/main/ProtectedRoute";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<LoggedInShell/>}>
-            <Route index path="create-links" element={<CreateLinks/>}/>
-            <Route path="profile" element={<Profile/>}/>
-        </Route>
-          <Route path="login" element={<Login/>} />
-          <Route path="create-account" element={<CreateActtount/>}/>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <AuthProvider>
+          <Routes>
+            <Route path='/' element={
+                <ProtectedRoute>
+                  <LoggedInShell/>
+                </ProtectedRoute>
+                }>
+                <Route index path="/" element={<CreateLinks/>}/>
+                <Route path="profile" element={<Profile/>}/>
+            </Route>
+              <Route path="login" element={<Login/>} />
+              <Route path="create-account" element={<CreateActtount/>}/>
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
   );
 }
 
